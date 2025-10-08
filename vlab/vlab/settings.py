a1146352a235d6987014cfb88aeb39ffbed1f447
sidd1224 <siddhu12smp@gmail.com>
2025-10-08 13:50:12 +0530

    third  commit

--- vlab/vlab/settings.py
+++ vlab/vlab/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -152,8 +151,5 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # or os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'static'] 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # your local static folder
+STATICFILES_DIRS = [BASE_DIR / 'static']
 AUTH_USER_MODEL = 'core.CustomUser'
